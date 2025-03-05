f2ab75aa635f1001ccd0ef1b1bc76c4d6f535ae8
Joseph Sepich <joe@sepich.dev>
2025-03-05 15:30:32 -0500

    DRY up code

--- train.py
+++ train.py
@@ -2,12 +2,21 @@ import carla
 import logging
 import time
 
-from camera import listener
+from camera import Camera
 
 MAP_NAME = 'Town03'
 SPAWN_POINT_IDX = 250
 IMAGE_WIDTH = 800
 IMAGE_HEIGHT = 600
+
+def spawn_actor_by_point(world, actor_name, spawn_point_idx):
+    blueprint_library = world.get_blueprint_library()
+    charger_blueprint = blueprint_library.filter(actor_name)[0]
+    spawn_points = world.get_map().get_spawn_points()
+    logging.debug(f'Found {len(spawn_points)} spawn points.')
+    spawn_point = spawn_points[spawn_point_idx]
+    logging.info(f'Spawning {charger_blueprint.id} at {spawn_point}')
+    return world.spawn_actor(charger_blueprint, spawn_point)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -30,28 +39,13 @@ if __name__ == '__main__':
         # client.set_timeout(2.0)
         # logging.info(f'Loaded {MAP_NAME} map.')
 
-        # Get car blueprint
-        blueprint_library = world.get_blueprint_library()
-        charger_blueprint = blueprint_library.filter('charger_police')[0]
-
-        # Get spawn point
-        spawn_points = world.get_map().get_spawn_points()
-        logging.info(f'Found {len(spawn_points)} spawn points.')
-
-        # Spawn vehicle
-        spawn_point = spawn_points[SPAWN_POINT_IDX]
-        logging.info(f'Spawning {charger_blueprint.id} at {spawn_point}')
-        charger = world.spawn_actor(charger_blueprint, spawn_point)
+        # Spawn vehicle]
+        charger = spawn_actor_by_point(world, 'charger_police', SPAWN_POINT_IDX)
         actor_list.append(charger)
 
         # Spawn vehicle sensor(s)
         logging.info('Spawning an RGB camera sensor on the charger')
-        cam_bp = blueprint_library.find('sensor.camera.rgb')
-        cam_bp.set_attribute('image_size_x', f'{IMAGE_WIDTH}')
-        cam_bp.set_attribute('image_size_y', f'{IMAGE_HEIGHT}')
-        cam_bp.set_attribute('fov', '110')
-        cam = world.spawn_actor(cam_bp, carla.Transform(carla.Location(x=1.5, z=2.4)), attach_to=charger) # transform relative to vehicle
-        cam.listen(listener)
+        cam = Camera(charger, world, IMAGE_WIDTH, IMAGE_HEIGHT).cam
         actor_list.append(cam)
 
         charger.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
